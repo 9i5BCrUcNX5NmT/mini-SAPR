2e1e78328c847c3a238496e9bd3779a1f19dedea
9i5BCrUcNX5NmT <machinima40@yandex.ru>
2025-09-15 14:43:15 +0300

    split botton system to many buttons + add button to spawn cube

--- src/main.rs
+++ src/main.rs
@@ -13,55 +13,52 @@ const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
 const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
 const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
 
+#[derive(Component)]
+struct Id(u32);
+
 #[derive(Resource)]
 struct CameraState {
     moved: bool,
 }
 
 fn button_system(
-    mut interaction_query: Query<
-        (
-            &Interaction,
-            &mut BackgroundColor,
-            &mut BorderColor,
-            &Children,
-        ),
-        (Changed<Interaction>, With<Button>),
-    >,
-    mut text_query: Query<&mut Text>,
+    mut interaction_query: Query<(&Interaction, &Id), (Changed<Interaction>, With<Button>)>,
     mut query_camera: Query<&mut Transform, With<Camera>>,
     mut state_camera: ResMut<CameraState>,
+
+    // to spawn cube
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    for (interaction, mut color, mut border_color, children) in &mut interaction_query {
-        let mut text = text_query.get_mut(children[0]).unwrap();
+    for (interaction, id) in &mut interaction_query {
         match *interaction {
-            Interaction::Pressed => {
-                **text = "Press".to_string();
-                *color = PRESSED_BUTTON.into();
-                border_color.0 = RED.into();
-
-                for mut transform in &mut query_camera {
-                    if !state_camera.moved {
-                        *transform =
-                            Transform::from_xyz(0.0, 0.0, 18.0).looking_at(Vec3::ZERO, Vec3::Y);
-                    } else {
-                        *transform =
-                            Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y);
+            Interaction::Pressed => match id.0 {
+                1 => {
+                    for mut transform in &mut query_camera {
+                        if !state_camera.moved {
+                            *transform =
+                                Transform::from_xyz(0.0, 0.0, 18.0).looking_at(Vec3::ZERO, Vec3::Y);
+                        } else {
+                            *transform =
+                                Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y);
+                        }
                     }
-                }
 
-                state_camera.moved = !state_camera.moved;
-            }
-            Interaction::Hovered => {
-                **text = "Hover".to_string();
-                *color = HOVERED_BUTTON.into();
-                border_color.0 = Color::WHITE;
-            }
-            Interaction::None => {
-                **text = "Button".to_string();
-                *color = NORMAL_BUTTON.into();
-                border_color.0 = Color::BLACK;
-            }
+                    state_camera.moved = !state_camera.moved;
+                }
+                2 => {
+                    // cube
+                    commands.spawn((
+                        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+                        MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
+                        Transform::from_xyz(0.0, 0.5, 0.0),
+                    ));
+                }
+                _ => {}
+            },
+            Interaction::Hovered => {}
+            Interaction::None => {}
         }
     }
 }
@@ -70,7 +67,6 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    assets: Res<AssetServer>,
 ) {
     // circular base
     commands.spawn((
@@ -78,12 +74,7 @@ fn setup(
         MeshMaterial3d(materials.add(Color::WHITE)),
         Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
     ));
-    // cube
-    commands.spawn((
-        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-        MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
-        Transform::from_xyz(0.0, 0.5, 0.0),
-    ));
+
     // light
     commands.spawn((
         PointLight {
@@ -101,14 +92,15 @@ fn setup(
 
     commands.insert_resource(CameraState { moved: false });
 
-    commands.spawn(button(&assets));
+    commands.spawn(button("Camera".into(), 1, 100.0, 100.0));
+    commands.spawn(button("Spawn Cube".into(), 2, 50.0, 50.0));
 }
 
-fn button(asset_server: &AssetServer) -> impl Bundle + use<> {
+fn button(name: String, id: u32, x: f32, y: f32) -> impl Bundle + use<> {
     (
         Node {
-            width: Val::Percent(100.0),
-            height: Val::Percent(100.0),
+            width: Val::Percent(x),
+            height: Val::Percent(y),
             align_items: AlignItems::Baseline,
             justify_content: JustifyContent::Center,
             ..default()
@@ -129,7 +121,7 @@ fn button(asset_server: &AssetServer) -> impl Bundle + use<> {
             BorderRadius::MAX,
             BackgroundColor(NORMAL_BUTTON),
             children![(
-                Text::new("Button"),
+                Text::new(name),
                 TextFont {
                     // font: asset_server.load("fonts/FiraSans-Bold.ttf"),
                     font_size: 8.0,
@@ -137,53 +129,8 @@ fn button(asset_server: &AssetServer) -> impl Bundle + use<> {
                 },
                 TextColor(Color::srgb(0.9, 0.9, 0.9)),
                 TextShadow::default(),
-            )]
+            )],
+            Id(id),
         )],
     )
 }
-
-// #[derive(Component)]
-// struct Object;
-
-// #[derive(Component)]
-// struct Name(String);
-
-// pub struct HelloPlugin;
-
-// impl Plugin for HelloPlugin {
-//     fn build(&self, app: &mut App) {
-//         app.insert_resource(ObjectTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
-//         app.add_systems(Startup, add_objects);
-//         app.add_systems(Update, (update_objects, init_objects).chain());
-//     }
-// }
-
-// fn add_objects(mut commands: Commands) {
-//     commands.spawn((Object, Name("Line".to_string())));
-//     commands.spawn((Object, Name("Point".to_string())));
-//     commands.spawn((Object, Name("Cube".to_string())));
-// }
-
-// #[derive(Resource)]
-// struct ObjectTimer(Timer);
-
-// fn init_objects(
-//     time: Res<Time>,
-//     mut timer: ResMut<ObjectTimer>,
-//     query: Query<&Name, With<Object>>,
-// ) {
-//     if timer.0.tick(time.delta()).just_finished() {
-//         for name in &query {
-//             println!("Initializing *{}*...", name.0);
-//         }
-//     }
-// }
-
-// fn update_objects(mut query: Query<&mut Name, With<Object>>) {
-//     for mut name in &mut query {
-//         if name.0 == "Line Point" {
-//             name.0 = "Point on Line".to_string();
-//             break;
-//         }
-//     }
-// }
